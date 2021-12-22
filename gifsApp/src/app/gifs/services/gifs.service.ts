54428c15f2a59408d8d80617ee7837867a447a60
OPOII <41926039+OPOII@users.noreply.github.com>
2021-12-22 17:00:12 -0500

    show the result of the http request in the page

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -6,24 +6,28 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
-  private _historial: string[]=[];
-  private apiKey    :string='Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi';
+  private _historial: string[] = [];
+  private apiKey: string = 'Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi';
 
-  get historial(){{
-    return [...this._historial];
-  }}
-  constructor(private http: HttpClient){}
-
-  buscarGifs(query:string){
-    query=query.trim().toLocaleLowerCase();
-    if(!this._historial.includes(query)){
-      this._historial.unshift(query);
-      this._historial=this._historial.slice(0,10);
+  public resultados: any[] = [];
+  get historial() {
+    {
+      return [...this._historial];
     }
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi&q=Dragon ball Z&limit=10')
-    .subscribe((resp:any)=>{
-      console.log(resp.data);
-    })
+  }
+  constructor(private http: HttpClient) { }
+
+  buscarGifs(query: string) {
+    query = query.trim().toLocaleLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.slice(0, 10);
+    }
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi&q=${query}&limit=10`)
+      .subscribe((resp: any) => {
+        this.resultados=resp.data;
+        console.log(resp.data);
+      })
   }
 
 }
