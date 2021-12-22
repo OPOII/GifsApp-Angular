6be38b4805083f36e43ff1835c71c4ca91da630c
OPOII <41926039+OPOII@users.noreply.github.com>
2021-12-22 10:44:32 -0500

    gifs module components

--- gifsApp/src/app/app.module.ts
+++ gifsApp/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
