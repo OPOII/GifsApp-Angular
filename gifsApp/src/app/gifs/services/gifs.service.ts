f3b9fc31ff3d8aa37d2d79b6960ce3f5d8042939
OPOII <41926039+OPOII@users.noreply.github.com>
2021-12-22 16:17:51 -0500

    using http request

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,11 +7,12 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _historial: string[]=[];
+  private apiKey    :string='Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi';
 
   get historial(){{
-    
     return [...this._historial];
   }}
+  constructor(private http: HttpClient){}
 
   buscarGifs(query:string){
     query=query.trim().toLocaleLowerCase();
@@ -18,9 +20,10 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial=this._historial.slice(0,10);
     }
-    //Acorto la cantidad de elementos que van a ser mostrados
-    //En el historial
-    console.log(this._historial);
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi&q=Dragon ball Z&limit=10')
+    .subscribe((resp:any)=>{
+      console.log(resp.data);
+    })
   }
 
 }
