dc1008f192c9cdeb00243ec7cca28f7e4ce6a6f3
OPOII <41926039+OPOII@users.noreply.github.com>
2021-12-24 13:13:06 -0500

    use of the local storage

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -16,13 +16,18 @@ export class GifsService {
       return [...this._historial];
     }
   }
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    if(localStorage.getItem('historial')){
+      this._historial=JSON.parse(localStorage.getItem('historial'));
+    }
+   }
 
   buscarGifs(query: string) {
     query = query.trim().toLocaleLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.slice(0, 10);
+      localStorage.setItem('historial',JSON.stringify(this._historial));
     }
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi&q=${query}&limit=10`)
       .subscribe((resp) => {
