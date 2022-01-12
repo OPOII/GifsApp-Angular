6a995a0dd990983aa42a3a8ce44de5b92bf0add9
OPOII <41926039+OPOII@users.noreply.github.com>
2022-01-12 12:35:08 -0500

    adding animations and finalize the app

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
@@ -9,6 +9,7 @@ export class GifsService {
 
   private _historial: string[] = [];
   private apiKey: string = 'Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
 
   public resultados: Gif[] = [];
   get historial() {
@@ -29,10 +30,13 @@ export class GifsService {
       this._historial = this._historial.slice(0, 10);
       localStorage.setItem('historial',JSON.stringify(this._historial));
     }
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=Yd0Nl5NGYHSR85X2sryRdvjxJMe4ktWi&q=${query}&limit=10`)
+
+    const params=new HttpParams().set('api_key',this.apiKey).set('limit','10').set('q',query);
+
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
       .subscribe((resp) => {
         this.resultados=resp.data;
-        console.log(resp.data);
       })
   }
 
